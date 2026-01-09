9c0b9f7a731d6258af79c44c522326274dfdb175
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 15:14:26 +0100

    vertaling functie PeriodicCard

--- frontend/src/components/cards/PeriodicCard.jsx
+++ frontend/src/components/cards/PeriodicCard.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import LanguageSwitcher from "../../components/layout/LanguageSwitcher";
+import { useTranslation } from "../../i18n/useTranslation";
+import { useLanguage } from "../../i18n/useLanguage";
 
 function PeriodicCard() {
+  const { language, letLanguage } = useLanguage();
+  const { t } = useTranslation();
+
   return (
     <article className="bg-white rounded-xl shadow-md px-8 py-6 min-h-[260px] w-full">
-      <h3 className="text-base font-medium mb-4">Periodieke reinigingen</h3>
+      <h3 className="text-base font-medium mb-4">{t("periodicCard.title")}</h3>
 
       <div className="flex gap-2 text-sm mb-1">
-        <span className="text-gray-600 w-20">Wekelijks:</span>
+        <span className="text-gray-600 w-20">{t("periodicCard.weekly")}</span>
         <span className="font-semibold">
-          Dinsdag 4/11, Vrijdag 15/11, Woensdag 20/11, Donderdag 27/11
+          Dinsdag 1/11, Vrijdag 9/11, Woensdag 16/11, Donderdag 27/11
         </span>
       </div>
 
       <div className="flex gap-2 text-sm">
-        <span className="text-gray-600 w-20">Maandelijks:</span>
+        <span className="text-gray-600 w-20">{t("periodicCard.monthly")}</span>
         <span className="font-semibold">Maandag 17/11</span>
       </div>
     </article>
