d48f13aab6c4d0ccf2c0ce8557038060c6335973
halloikbenleerling <digay.kengoum@student.odisee.be>
2026-01-11 13:34:59 +0100

    Update server.js
    Was hardcoded

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,12 @@ import allboxesRoutes from "./src/routes/allboxesRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5174'],
+  origin: [`${FRONTEND_URL}`],
   credentials: true
 }));
 app.use(generalLimiter);
